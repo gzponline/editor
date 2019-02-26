7bc7d597cbc970b6c6785b77436db9dd5f4abefe
外城士心月月鸟 <gzponline@163.com>
2019-02-26 11:41:22 +0800

    add 啊有点多改动

--- editor.js
+++ editor.js
@@ -33,120 +33,219 @@ const basic = {
 	getPPosition(event){
 		let x = event.pageX || (event.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft));
 		let y = event.pageY || (event.clientY + (document.documentElement.scrollTop || document.body.scrollTop));
-		return {'x':x,'y':y};
+		return {'x':x ,'y':y};
 	},
 	exec: (command, value = null) => {
+        console.log(123)
+        setWord.restoreSelection()
 		document.execCommand(command, false, value);
 	},
-	queryCommandState: command => document.queryCommandState(command)
+    queryCommandState: command => document.queryCommandState(command),
+    queryCommandValue: command => document.queryCommandValue(command),
 
 };
 
-const defaultWordList = [
-	"1 01234567890/11234567890/<br />21234567890<i>31234567890</i>41234567890<br />我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师<br />我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师</p>",
-	"2 0 1234567890/11234567890/<br />21234567890<i>31234567890</i>41234567890<br />我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师<br />我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师</p>",
-	"3 0 1234567890/11234567890/<br />21234567890<i>31234567890</i>41234567890<br />我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师<br />我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师我是一名工程师</p>",
-	"<div>wosiyshi </div>",
-	"0小强zaxsdcfvgbhnujimk",
-	"1小强azwsdtgbynujmi,o",
-	"2小明zaqwxsdcvfgbhnujiko",
+const testWordList = [
+	"第一段 测试文字<div>测试内嵌div1</div>换行<br/><div>测试内嵌div2</div>",
+	"第二段 测试文字<div>测试内嵌div1</div>换行<br/><div>测试内嵌div2</div>",
+	"第三段 链接外前文字<a href= 'https://juejin.im/user/5a8a698cf265da4e896ad8ca'id ='test'> 测试自带链接</a>链接外文字</p>",
+	"第四段 <div>div内文字</div>",
+	"0小强",
+	"1小强",
+	"2小明",
 	"3小红",
-	"4小绿ìzawsxedcrfvtgbyhnujmik",
-	"5小白zaqwsxedcrfvtgbyhnujmi",
-	"6小紫",
-	"7小强zawsxedcrfvtgbyujmi",
-	"8小明azqwsxecdrfvtbgyhnujmik,o",
-	"9小红zqawxsedcrfvtgbyhnujmiko,,,,,l"
+	"4小绿",
+	"5小白",
+	"第九段测试 链接外前文字<a href= 'https://juejin.im/user/5a8a698cf265da4e896ad8ca'id ='test'> 测试自带链接</a>链接外文字第九段测试 链接外前文字<a href= 'https://juejin.im/user/5a8a698cf265da4e896ad8ca'id ='test'> 测试自带链接</a>链接外文字第九段测试 链接外前文字<a href= 'https://juejin.im/user/5a8a698cf265da4e896ad8ca'id ='test'> 测试自带链接</a>链接外文字</p>",
+	"7小强",
+	"8小明",
+	"9小红"
 ];
 
-const setWord = {
+const state = {
 	element:"",
 	selectColor:"#FFF",
-	selectWord:"",
-	colors:["blue","green","red","yellow","grey"],
-	begin(ele){
-		if(ele){
-			this.element = document.getElementById(ele);
-		}
-		if(this.element){
-			this.element.contentEditable = true;
-			basic.addEvent(this.element,"mousedown",this.bind.selectWordStart);
+    selectWord:"",
+    selectedRange: "",
+    link: () => {
+        return setWord.getSelectedLink()
+    },
+	showToolsbar: false,
+};
+const defaultActions = {
+    bold: {
+      icon: '<b>B</b>',
+      title: 'Bold',
+      state: () => basic.queryCommandState('bold'),
+      result: () => basic.exec('bold')
+    },
+    italic: {
+      icon: '<i>I</i>',
+      title: 'Italic',
+      state: () => basic.queryCommandState('italic'),
+      result: () => basic.exec('italic')
+    },
+    strikethrough: {
+      icon: '<strike>S</strike>',
+      title: 'Strike-through',
+      state: () => basic.queryCommandState('strikeThrough'),
+      result: () => basic.exec('strikeThrough')
+    },
+    heading1: {
+      icon: '<b>H<sub>1</sub></b>',
+      title: 'Heading 1',
+      result: () => basic.exec("formatBlock", '<h1>')
+    },
+    heading2: {
+      icon: '<b>H<sub>2</sub></b>',
+      title: 'Heading 2',
+      result: () => basic.exec("formatBlock", '<h2>')
+    },
+    line: {
+      icon: '&#8213;',
+      title: 'Horizontal Line',
+      result: () => basic.exec('insertHorizontalRule')
+    },
+    link: {
+        icon: '<span style="text-decoration: underline;">Link</span>',
+        title: 'Link',
+        state: ()=> setWord.getSelectedLink(),
+        result: () => {
+
+        }
+      },
+}
+const setWord = {
+    begin(){
+	    let editor = document.getElementById("editor");
+		if(editor){
+			editor.contentEditable = true;
+			basic.addEvent(editor,"mousedown",this.selectWordStart);
 			
 			let chooserColorDiv = document.getElementById("chooserColor");
-			let creatS = document.getElementById("creatS")
 			let insertLink = document.getElementById("insertLink") 
 			
-			basic.addEvent(chooserColorDiv, "click",  setWord.createDiv.liclick)
-			basic.addEvent(creatS, "click", setWord.createDiv.hideDiv)
-			// basic.addEvent(insertLink, "click", setWord.createDiv.addLink)
-
+			basic.addEvent(chooserColorDiv, "click",  setWord.liclick)
+            basic.addEvent(insertLink, "click", setWord.addLink)
+            // 添加工具栏
+            let creatU = document.getElementById("creatU")
+            const actions = Object.keys(defaultActions).map(action => defaultActions[action]);
+            actions.forEach( action => {
+                const button = document.createElement('li')
+                button.className = "action"
+                button.innerHTML = action.icon
+                button.title = action.title
+                button.onclick = () => action.result() && content.focus()
+            
+                if (action.state) {
+                  const handler = () => button.classList[action.state() ? 'add' : 'remove']("yes")
+                  basic.addEvent(creatU, 'keyup', handler)
+                  basic.addEvent(creatU, 'mouseup', handler)
+                  basic.addEvent(button, 'click', handler)
+                }
+                creatU.append(button)
+            })
 		}else{
 			alert("绑定失败，请检查是否存在 id= "+ele+" 的元素！");
 		}
+    },
+    selectWordStart(){
+	    let editor = document.getElementById("editor");
+        basic.addEvent(editor,"mouseup",setWord.selectWordEnd);
 	},
-};
-
-setWord.createDiv = {
-	showDiv(event){  
-		let chooserColorDiv = document.getElementById("chooserColor");
-		chooserColorDiv.style.display = "inline"
-		let Position = basic.getPPosition(event);
-		chooserColorDiv.style.top = Position.y-0+10+"px";
-		chooserColorDiv.style.left = (Position.x-5)+"px";
-	},
-	hideDiv(){   
-		if("" != document.getElementById("chooserColor")){
-			document.getElementById("chooserColor").style.display = "none"
-		}
-	},
-	liclick(event){
-			if(event.target.tagName.toLowerCase() == "button"){
-				let getBackgroundColor = event.target.getAttribute("tar");
-				setWord.selectColor = getBackgroundColor;
-				setWord.createDiv.hideDiv();
-				setWord.createDiv.changeWordColor();
-			}
-	},
-	changeWordColor(){     
-		if(!setWord.selectWord){
-			alert("您还没有选择词语，请选择！");
-		}else{
-			basic.exec("backColor", setWord.selectColor);
-			//变色完成，把相关的参数初始化。
-			setWord.selectColor = "#F1F";
-			setWord.selectWord = "";
-		}
-	},
-	// addLink(event){
-	// 	let link = document.getElementById("linkInput").value
-	// 	basic.exec("createLink", true, link);
-	// },
-	
-};
-setWord.bind = {
-	selectWordStart(){
-		basic.addEvent(setWord.element,"mouseup",setWord.bind.selectWordEnd);
-	},
-	selectWordEnd(event){     
+	selectWordEnd(event){
+        setWord.saveSelection()
 		let word = "";
 		// IE but I not have  IE
 		if(document.selection){
 			let sel = document.selection.createRange();
 			word = sel.text;
-			setWord.selectWord = sel;
+			state.selectWord = sel;
 		}else if(window.getSelection){ // chrome or firfox
 			word=window.getSelection();
-			setWord.selectWord = window.getSelection().getRangeAt(0);
+			state.selectWord = window.getSelection().getRangeAt(0);
 		}
-		// 当鼠标选中的内容不为空 或者 鼠标右键单击时候
+		// 当鼠标选中的内容不为空
 		if(word != "" ){
-			// || event.button == 2
-			setWord.createDiv.showDiv(event);
-		}
-		
+			setWord.showDiv(event);
+        }
+        // 更新链接状态
+        document.querySelector("#linkInput").value = state.link()? state.link(): "";
+        setWord.restoreSelection()
+    },
+	showDiv(event){  
+        let chooserColorDiv = document.getElementById("chooserColor");
+		let Position = basic.getPPosition(event);
+        chooserColorDiv.style.display = 'block'
+		chooserColorDiv.style.top = Position.y-0+10+"px";
+        chooserColorDiv.style.left = (Position.x-5)+"px";
 	},
+	hideDiv(){   
+		if("" != document.getElementById("chooserColor")){
+			document.getElementById("chooserColor").style.display = "none"
+        }
+        state.showToolsbar = false
+	},
+	liclick(event){
+			if(event.target.tagName.toLowerCase() == "li"){
+				let getBackgroundColor = event.target.getAttribute("tar");
+				state.selectColor = getBackgroundColor;
+				setWord.hideDiv();
+				setWord.changeWordColor();
+			}
+	},
+	changeWordColor(){
+        setWord.restoreSelection()
+		if(!state.selectWord){
+			alert("您还没有选择词语，请选择！");
+		}else{
+			basic.exec("backColor", state.selectColor);
+			//变色完成，把相关的参数初始化。
+			state.selectColor = "#F1F";
+			state.selectWord = "";
+		}
+	},
+	addLink(){
+        // todo 选中嵌套链接时候的处理
+        setWord.restoreSelection()
+        let link = document.getElementById("linkInput").value
+        console.log(link)
+        basic.exec("createLink", link);
+    },
+    unlink(){
+        
+    },
+    getSelectedLink() {
+        // 获取选区内是否包含链接 或者包括链接标题的部分文字
+        if(window.getSelection().anchorNode ||window.getSelection().focusNode){
+            var anchorNodeHref = window.getSelection().anchorNode.parentNode.href; // 获取选区开始处光标 后 一个字所在的节点
+            var focusNodeHref = window.getSelection().focusNode.parentNode.href; // 获取选区结束处光标 前 一个字所在的节点
+            if (anchorNodeHref || focusNodeHref){
+                var link = anchorNodeHref || focusNodeHref
+                return link
+            }
+        }
+    },
 	
-	
+    saveSelection() {
+        var sel = window.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+            state.selectedRange = sel.getRangeAt(0);
+        }
+    },
+    restoreSelection() {
+        // 恢复选区状态
+        var selection = window.getSelection();
+        if (state.selectedRange) {
+            try {
+                selection.removeAllRanges();
+            } catch (ex) {
+                document.body.createTextRange().select();
+                document.selection.empty();
+            }
+            selection.addRange(state.selectedRange);
+        }
+    },
 };
 setWord.begin("editor");
 
@@ -155,7 +254,7 @@ const holdItemDom = document.createElement("div");
 holdItemDom.classList.add("item", "hold");
 const listDom = document.createElement("div");
 listDom.classList.add("items");
-defaultWordList.forEach((v, i, arr) => {
+testWordList.forEach((v, i, arr) => {
 	const itemDom = document.createElement("div");
 	itemDom.classList.add("item");
 	itemDom.innerHTML = v;
@@ -168,63 +267,67 @@ document.querySelector("#editor").appendChild(listDom);
 let dragObj, enterObj, dragIndex, enterIndex;
 const itemsObj = document.querySelector(".items");
 
-basic.addEvent(itemsObj,"dragstart",(event) => {
-	dragObj = event.target;
-	const itemDomList = document.querySelectorAll(
-		".items .item"
-	);
-	itemDomList.forEach((dom, index) => {
-		if (dom === dragObj) {
-			dragIndex = index;
-		}
-	});
-	event.target.style.opacity = 0.5;
-})
-basic.addEvent(itemsObj,"dragend",(event) => {
-	event.target.style.opacity = "";
-})
-
-basic.addEvent(itemsObj,"dragover",(event) => {
-	event.preventDefault();
-})
-
-basic.addEvent(itemsObj,"dragenter",(event) => {
-	if (event.target.className === "item") {
-		event.target.classList.add("hold");
-	}
-	enterObj = event.target;
-	const itemDomList = document.querySelectorAll(
-		".items .item"
-	);
-	itemDomList.forEach((dom, index) => {
-		if (dom === enterObj) {
-			enterIndex = index;
-		}
-	});
-})
-
-basic.addEvent(itemsObj,"dragleave",(event) => {
-	if (/hold/.test(event.target.classList)) {
-		event.target.classList.remove("hold");
-	}
-})
-
-basic.addEvent(itemsObj,"drop",(event) => {
-	event.preventDefault();
-	enterObj.classList.remove("hold");
-	if(event.target.className !== "item"){
-		return
-	}
-	if (dragIndex < enterIndex) {
-		dragObj.remove();
-		enterObj.after(dragObj);
-	} else if (dragIndex > enterIndex) {
-		dragObj.remove();
-		enterObj.before(dragObj);
-	}
-})
-
-basic.addEvent(document,"dragexit",(event) => {
-	event.preventDefault();
-
-})
+const addEventList = {
+    "dragstart": (event) => {
+        dragObj = event.target;
+        const itemDomList = document.querySelectorAll(
+            ".items .item"
+        );
+        itemDomList.forEach((dom, index) => {
+            if (dom === dragObj) {
+                dragIndex = index;
+            }
+        });
+        if (event.target.className === "item") {
+            event.target.style.opacity = 0.5;
+        }
+    },
+    "dragend":(event) => {
+        if (event.target.className === "item") {
+            event.target.style.opacity = "";
+        }
+    },
+    "dragover": (event) => {
+        event.preventDefault();
+    },
+    "dragenter": (event) => {
+        if (event.target.className === "item") {
+            event.target.classList.add("hold");
+        }
+        enterObj = event.target;
+        const itemDomList = document.querySelectorAll(
+            ".items .item"
+        );
+        itemDomList.forEach((dom, index) => {
+            if (dom === enterObj) {
+                enterIndex = index;
+            }
+        });
+    },
+    "dragleave": (event) => {
+        if (/hold/.test(event.target.classList)) {
+            event.target.classList.remove("hold");
+        }
+    },
+    "drop": (event) => {
+        event.preventDefault();
+        enterObj.classList.remove("hold");
+        if(event.target.className !== "item"){
+            return
+        }
+        if (dragIndex < enterIndex) {
+            dragObj.remove();
+            enterObj.after(dragObj);
+        } else if (dragIndex > enterIndex) {
+            dragObj.remove();
+            enterObj.before(dragObj);
+        }
+    }
+}
+Object.keys(addEventList).map(
+    addEventName =>{
+        basic.addEvent(itemsObj,addEventName,(event) => {
+            addEventList[addEventName](event);
+        })
+    }
+)
